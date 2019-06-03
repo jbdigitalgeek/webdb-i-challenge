36ed7ba9d07be83de47f5deaa613b59432336f2c
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-03 14:59:47 -0700

    get endpoints working

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 
+const Accounts = require('./data/accounts-model');
+
 const server = express();
 
-// your code here
+server.get('/', async (req, res) => {
+    try {
+        const acctList = await Accounts.find();
+        res.status(200).json(acctList);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: `${err}` })
+    }
+});
+
+server.get('/:id', async (req, res) => {
+    try {
+        const acctId = await Accounts.findById(req.params.id);
+        res.status(200).json(acctId)
+    } catch (error) {
+        res.status(500).json({ message: `${error}` })
+    }
+});
 
 module.exports = server;
